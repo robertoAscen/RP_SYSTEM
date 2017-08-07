6928f3bca9f6b33f2c4081998ec036b91b45a17f
Roberto Ascencio <roberto_ascencio@eneri.com.mx>
2017-08-07 17:48:11 -0500

    Se agregarón los if correspondiente a cada boton disponible en esta ventana

--- src/main/java/com/mti/drugstore/controlador/principal/CtrlMenuPrincipal.java
+++ src/main/java/com/mti/drugstore/controlador/principal/CtrlMenuPrincipal.java
@@ -5,12 +5,14 @@
  */
 package com.mti.drugstore.controlador.principal;
 
+import com.mti.drugstore.controlador.producto.CtrlAgregarProducto;
 import com.mti.drugstore.controlador.producto.CtrlBuscarProducto;
 import com.mti.drugstore.modelos.DaoFormula;
 import com.mti.drugstore.modelos.DaoMedicamento;
 import com.mti.drugstore.modelos.DaoPresentacion;
 import com.mti.drugstore.modelos.DaoSustanciaAct;
 import com.mti.drugstore.vista.principal.VistaMenuPrincipal;
+import com.mti.drugstore.vista.producto.VistaAgregarProducto;
 import com.mti.drugstore.vista.producto.VistaBuscarProducto;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -68,13 +70,17 @@ public class CtrlMenuPrincipal implements ActionListener
     {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         
-        if(e.getSource() == mainMenu.jb_1)
-        { 
-                VistaBuscarProducto viewProducts = new VistaBuscarProducto();
-                DaoMedicamento medDAO = new DaoMedicamento();
-                DaoPresentacion presentacionDAO = new DaoPresentacion();
-                CtrlBuscarProducto ctrlFindProduct = new CtrlBuscarProducto(viewProducts, medDAO, presentacionDAO);
-                viewProducts.setVisible(true);
+        if(e.getSource() == mainMenu.jmiAdmonUser)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiDB)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiConfig)
+        {
+            
         }
         if(e.getSource() == mainMenu.jmiExit)
         {
@@ -91,6 +97,160 @@ public class CtrlMenuPrincipal implements ActionListener
                 case 2:
                     break;
             }
+        }    
+        if(e.getSource() == mainMenu.jmiProduct)
+        {
+            VistaAgregarProducto createProduct = new VistaAgregarProducto();
+            DaoMedicamento med_DAO = new DaoMedicamento();
+            DaoPresentacion presentDAO = new DaoPresentacion();
+            DaoSustanciaAct susDAO = new DaoSustanciaAct();
+            DaoFormula formDAO = new DaoFormula();
+            CtrlAgregarProducto ctrlCreateProduct = new CtrlAgregarProducto(createProduct, med_DAO, presentDAO, susDAO, formDAO);
+            createProduct.setVisible(true);  
+        }
+        if(e.getSource() == mainMenu.jmiVendor)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiCliente)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiDaySales)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiSalesXProduct)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiBestProduct)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiReportXMonthXProduct)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiReportXMonthXCustomer)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiReportXYearXProduct)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiReportXYearXCustomer)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiListOfProducts)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiListOfVendors)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiListOfCustomers)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiGraphBar)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiGraphLines)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiGraphCake)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiDoInventario)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiProductFaltante)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiProductSobrante)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiPedidos)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiFacturasEmitidas)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiPagoNomina)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiRetiro)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiCierre)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiVendedor)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiMedico)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiInfo)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jmiFace)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jb_1)
+        { 
+                VistaBuscarProducto viewProducts = new VistaBuscarProducto();
+                DaoMedicamento medDAO = new DaoMedicamento();
+                DaoPresentacion presentacionDAO = new DaoPresentacion();
+                CtrlBuscarProducto ctrlFindProduct = new CtrlBuscarProducto(viewProducts, medDAO, presentacionDAO);
+                viewProducts.setVisible(true);
+        }   
+        if(e.getSource() == mainMenu.jb_2)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jb_3)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jb_4)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jb_5)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jb_6)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jb_7)
+        {
+            
+        }
+        if(e.getSource() == mainMenu.jb_8)
+        {
+            
         }
     }
 }
